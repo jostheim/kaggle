7e566bb730e2ba877e651500be8a757c53354de8
James Ostheimer <james.ostheimer@gmail.com>
2013-01-12 17:21:29 -0600

    pickle doesn't work so have to shoot the moon

--- src/flight_quest/flight_quest_panda.py
+++ src/flight_quest/flight_quest_panda.py
@@ -701,8 +701,10 @@ if __name__ == '__main__':
         all_df = process_into_features(all_df, unique_cols)
         print all_df
 #        # may want to rebin here
-        targets = all_df['gate_arrival_diff'].dropna().apply(lambda x: int(x) if x is not np.nan else np.nan)
+        targets = all_df['gate_arrival_diff'].dropna()
+        print targets
         features = all_df.ix[all_df['gate_arrival_diff'].dropna()]
+        print features
         random_forest_classify(targets, features)
         
 

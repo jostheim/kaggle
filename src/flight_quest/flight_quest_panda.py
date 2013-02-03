845e3bd771a7c80a32b5eb231806944445479821
James Ostheimer <james.ostheimer@gmail.com>
2013-02-02 20:02:29 -0600

    fixing up stuff for augmented data set and for atscc not existing

--- src/flight_quest/flight_quest_panda.py
+++ src/flight_quest/flight_quest_panda.py
@@ -669,7 +669,7 @@ def get_atscc_delay(data_prefix, data_rev_prefix, date_prefix, cutoff_time=None)
     if cutoff_time is not None:
         atsccdelay_df = atsccdelay_df[atsccdelay_df['capture_time'] < cutoff_time]
         for ix in atsccdelay_df[atsccdelay_df['end_time'] > cutoff_time].index:
-            atsccdelay_df,ix[ix]['end_time'] = np.nan
+            atsccdelay_df.ix[ix]['end_time'] = np.nan
         for ix in atsccdelay_df[atsccdelay_df['invalidated_time'] > cutoff_time].index:
             atsccdelay_df.ix[ix]['invalidated_time'] = np.nan
     cast_date_columns(atsccdelay_df, atscc_delay_date_cols)

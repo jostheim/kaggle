7e617c019b79ae90501035a9921b2907fb0897b6
James Ostheimer <james.ostheimer@gmail.com>
2013-02-14 00:16:39 -0600

    concat features

--- src/flight_quest/flight_quest_panda.py
+++ src/flight_quest/flight_quest_panda.py
@@ -916,7 +916,7 @@ def get_joined_data(data_prefix, data_rev_prefix, date_prefix, store_filename, f
     if df is None:
         print "Working on {0}".format(date_prefix)
         asdi_merged = get_asdi_merged(data_prefix, data_rev_prefix, date_prefix, cutoff_time=cutoff_time) 
-        print asdi_merged['estimateddepartureutc_diff'].dropna()
+#        print asdi_merged['estimateddepartureutc_diff'].dropna()
         df = get_flight_history(data_prefix, data_rev_prefix, date_prefix, cutoff_time=cutoff_time)
         events = get_flight_history_events(df, data_prefix, data_rev_prefix, date_prefix, cutoff_time=cutoff_time)
         asdi_disposition = get_asdi_disposition(data_prefix, data_rev_prefix, date_prefix, cutoff_time=cutoff_time)

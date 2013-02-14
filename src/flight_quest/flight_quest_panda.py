fd3b5f140390dbf7ddfea92b1b057f8529969b55
James Ostheimer <james.ostheimer@gmail.com>
2013-02-13 21:46:51 -0600

    concat features

--- src/flight_quest/flight_quest_panda.py
+++ src/flight_quest/flight_quest_panda.py
@@ -1501,7 +1501,7 @@ def generate_features(learned_class_name, store):
     write_dataframe("features_{0}".format(learned_class_name), all_df, store)
 
 def cross_validate(learned_class_name):
-    all_df = pd.read_csv("features_{0}.csv".format(learned_class_name), nrows=5000) 
+    all_df = pd.read_csv("features_{0}.csv".format(learned_class_name), nrows=2000) 
     all_df.set_index("flight_history_id", inplace=True, verify_integrity=True)
     # fix screw up with index column
     if "ind" in all_df.columns:
